3f2b24c15cc93a3801e5e52dea2c11740d009e92
kev2575 <124090093+kev2575@users.noreply.github.com>
2024-09-18 14:16:37 -0400

    Update server.js
    added new endpoint to search for movies

--- Backend/server.js
+++ Backend/server.js
@@ -9,18 +9,22 @@ let db = new sqlite3.Database('./CinemaApp.db', sqlite3.OPEN_READWRITE, (err) =>
   console.log('Connected to the  SQlite database.');
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
-});
-
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
-app.get('/about', (req, res) => {
-    res.send('This is the about page');
-  });
 app.get('/movie/trailers', (req, res) => {
   db.all('SELECT movie_title,trailer_link FROM Movies', (err, rows) => {
+      if (err) {
+          res.status(500).json({ error: err.message });
+      } else {
+          res.json(rows);
+      }
+  });
+});
+app.get('/movie/search', (req, res) => {
+  let inputString = req.body.string;
+  let querystring = 'SELECT movie_title, trailer_picture FROM Movies where movie_title like \'%' + inputString + '%\'';
+  db.all(querystring, (err, rows) => {
       if (err) {
           res.status(500).json({ error: err.message });
       } else {
