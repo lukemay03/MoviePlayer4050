cb1fcc3d71e32bc232ead94820d9fa5d2bad4de5
lhm88539 <lhm88539@uga.edu>
2024-09-20 12:22:03 -0400

    fixed merge conflict

--- movie_app/src/EditMovie.js
+++ movie_app/src/EditMovie.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import AdminHeader from './AdminHeader';
+import AdminHeader from './components/AdminHeader';
 
 function EditMovie() {
     return (
