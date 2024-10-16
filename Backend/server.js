6127eea184b496215146d37c43b09a64945004b3
kev2575 <124090093+kev2575@users.noreply.github.com>
2024-10-16 14:37:17 -0400

    add endpoint

--- Backend/server.js
+++ Backend/server.js
@@ -60,7 +60,7 @@ app.get('/movie/search', (req, res) => {
       }
   });
 });
-// Endpoint to insert a new user
+// Endpoint to insert a new movie
 app.post('/movie/insert', (req, res) => {
   console.log(req.body);
   let { moviename, cast, category, director, producer, synopsis, trailerpicture, trailerlink, movierating, current_running } = req.body;
@@ -68,6 +68,22 @@ app.post('/movie/insert', (req, res) => {
   // Insert the user into the database
   let sql = 'INSERT INTO Movies (movie_title, cast, category, director, producer, synopsis, trailer_picture, trailer_link, movie_rating, Current_running) VALUES (?,?,?,?,?,?,?,?,?,?)'
   db.run(sql, [moviename, cast, category, director,producer,synopsis,trailerpicture,trailerlink,movierating,current_running], (err) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send('Error inserting user');
+    } else {
+      res.status(201).send('User created successfully');
+    }
+  });
+});
+// Endpoint to insert a new user
+app.post('/user/insert', (req, res) => {
+  console.log(req.body);
+  let { role, first_name, last_name , email, password, status} = req.body;
+
+  // Insert the user into the database
+  let sql = 'INSERT INTO Users (role, first_name, last_name, email, password, status) VALUES (?,?,?,?,?,?)'
+  db.run(sql, [role, first_name, last_name, email,password,status], (err) => {
     if (err) {
       console.error(err.message);
       res.status(500).send('Error inserting user');
