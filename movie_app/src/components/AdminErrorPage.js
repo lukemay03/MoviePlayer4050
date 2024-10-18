9ccff47b1f36be7415a921d64fe3406ba5857c2e
kev2575 <124090093+kev2575@users.noreply.github.com>
2024-10-18 15:21:48 -0400

    fixed db

--- movie_app/src/components/AdminErrorPage.js
+++ movie_app/src/components/AdminErrorPage.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function AdminErrorPage() {
     return (
         <header className="site-header">
-            <h1>Not a admin</h1>
+            <h1>Sorry you must be a Admin to access this page</h1>
             <nav className='nav-links'>
                 <Link to="/">User Home</Link>
             </nav>
