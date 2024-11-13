6325429bcc9f6df0e92d788c140f8c377fc1773f
JacksonLGreer <jlg40585@uga.edu>
2024-11-13 17:02:42 -0500

    added order confirm email

--- movie_app/src/OrderConfirmPage.js
+++ movie_app/src/OrderConfirmPage.js
@@ -1,9 +1,72 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 
 
 
 function RegistrationConfirm() {
+
+  const [email, setEmail] = useState('');  
+  const [errorMessage, setErrorMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+
+  useEffect(() => {
+    //send token and get user's email
+    const fetchUserProfile = async () => {
+      try {
+        const response = await fetch('http://localhost:3001/user/profile', {
+          headers: {
+            'Authorization': `Bearer ${localStorage.getItem('token')}`,  
+          },
+        });
+
+        //parse response
+        const data = await response.json();
+        //console.log(data);
+        if (response.ok) {
+          setEmail(data.email);
+        } else {
+          setErrorMessage(data.message);
+        }
+      } catch (error) {
+        console.log(error.message)
+        setErrorMessage('Error fetching user data');
+      }
+    };
+
+    fetchUserProfile();
+  }, []);
+
+  // This function sends a request to the backend to send the email
+  const sendConfirmationEmail = async () => {
+    try {
+        const htmlMessage = "<h2>Thank you for ordering!</h2>" +
+            "<p>Your order is confirmed. Welcome to Movie Player Co!</p>" +
+            "<p>Best regards,</p>" +
+            "Movie Player Co.";
+        const subject = "Order Confirmation";
+        const response = await fetch('http://localhost:3001/trigger-order-confirm', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email, htmlMessage, subject }), // passes the user email to the backend
+        });
+        const data = await response.json();
+        if (response.ok) {
+            console.log(data.message); // "Confirmation email sent."
+        } else {
+            console.error('Failed to send email:', data.message);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
+
+// Trigger email sending when the component loads
+useEffect(() => {
+  sendConfirmationEmail();
+}, []); // Empty dependency array ensures this runs only once when the component mounts
+
   return (
     <div className="register-container">
       <Header></Header>
